926f86f44eaf5add34aa0df86d3c55ca99e8e69e
Brett Varney <brett.varney@kc.frb.org>
2020-03-16 07:48:51 -0500

    don't return 404 frag if window object

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -43,38 +43,39 @@ const afgTranslation = old => {
 const NotFoundPage = () => {
 
   // check for Jekyll links, redirect if possible
-	const browser = typeof window !== "undefined";
+  if (typeof window !== 'undefined') {
+    let urlFrags;
+    if (window.location.href.indexOf('.html') > -1) {
+      urlFrags = splitUrl(window.location.href);
 
-  if (browser && window.location.href.indexOf('.html') > -1) {
-      const urlFrags = splitUrl(window.location.href);
-      const afg = urlFrags[0].indexOf('/americas-finance-guide/');
-
-      if (afg > -1) { // AFG pages have moved; can't simply strip ".html"
+      let afg;
+      if ((afg = urlFrags[0].indexOf('/americas-finance-guide/')) > -1) { // AFG pages have moved; can't simply strip ".html"
         window.location = (afgTranslation(urlFrags[0].slice(afg + 24)) + (urlFrags[1] ? '?' + urlFrags[1] : ''));
-        return<></>;
+        return <></>;
       }
       window.location = urlFrags[0] + (urlFrags[1] ? '?' + urlFrags[1] : '');
       return <></>;
 
+    } else {
+      return <HeaderOnly _containerClass='four-oh-four' _headerClass='four-oh-four__header'>
+        <SEO title='404: Not found' />
+        <div id='four-oh-four'>
+          <div className='four-container'>
+            <h1>Oops!</h1>
+            <h4>Looks like you got lost in the data.
+            <div><span>It happens.</span> Find your way back.</div>
+            </h4>
+            <div id='btn-container'>
+              <div id='back-btn'><a href='/'>BACK</a></div>
+              <div id='home-btn'><a href='/'>HOME</a></div>
+            </div>
+          </div>
+        </div>
+      </HeaderOnly>;
+    }
   } else {
-		return (
-		<HeaderOnly _containerClass='four-oh-four' _headerClass='four-oh-four__header'>
-				<SEO title='404: Not found'/>
-				<div id='four-oh-four'>
-					<div className='four-container'>
-						<h1>Oops!</h1>
-						<h4>Looks like you got lost in the data.
-							<div><span>It happens.</span> Find your way back.</div>
-						</h4>
-						<div id='btn-container'>
-							<div id='back-btn'><a href='/'>BACK</a></div>
-							<div id='home-btn'><a href='/'>HOME</a></div>
-						</div>
-					</div>
-				</div>
-			</HeaderOnly>
-		)
-	}
-};
+    return <></>;
+  }
+}
 
 export default NotFoundPage;
